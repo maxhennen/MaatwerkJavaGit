070c5768b43917427e9bae6612cbfd1c90de926e
maxhennen <max.hennen@planet.nl>
2017-04-10 15:01:50 +0200

    Calculator gemaakt

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
+import javax.swing.*;
+
 public class Controller {
+    public void Min(int Getal1,int Getal2){
+        int uitkomst = Getal1 - Getal2;
+        JOptionPane.showMessageDialog(null,"Uitkomst is: " + String.valueOf(uitkomst));
+    }
+
+    public void Plus(int Getal1,int Getal2){
+        int uitkomst = Getal1 - Getal2;
+        JOptionPane.showMessageDialog(null,"Uitkomst is: " + String.valueOf(uitkomst));
+    }
+
+    public void Devide(int Getal1,int Getal2){
+        int uitkomst = Getal1 / Getal2;
+        JOptionPane.showMessageDialog(null,"Uitkomst is: " + String.valueOf(uitkomst));
+    }
+
+    public void Multiply(int Getal1,int Getal2){
+        int uitkomst = Getal1 * Getal2;
+        JOptionPane.showMessageDialog(null,"Uitkomst is: " + String.valueOf(uitkomst));
+    }
+
+    public void Plus(ActionEvent actionEvent,int getal1, int getal2) {
+
+    }
 }
